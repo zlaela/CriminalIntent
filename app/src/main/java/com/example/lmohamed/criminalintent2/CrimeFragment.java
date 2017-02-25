0ce20dde712b4be5c39cc2e15576754f0043f667
lmohamed <lmohamed@lmohamed.local>
2017-02-25 00:59:01 -0500

    Send crime report to contact from phone
    Select a suspect for the nefarious deed from the contacts list and send
    a report from the app indicating the crime, the date of the act, the
    suspect, and whether it was solved.

--- app/src/main/java/com/example/lmohamed/criminalintent2/CrimeFragment.java
+++ app/src/main/java/com/example/lmohamed/criminalintent2/CrimeFragment.java
@@ -2,7 +2,11 @@ package com.example.lmohamed.criminalintent2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.text.Editable;
@@ -29,13 +33,14 @@ public class CrimeFragment extends Fragment {
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
     private static final int REQUEST_DATE = 0;
+    private static final int REQUEST_CONTACT = 1;
 
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckbox;
     private Button mSuspectButton;
-    private Button mSendReportButton;
+    private Button mReportButton;
 
 
     /** Attaching arguments to a fragment
@@ -102,7 +107,7 @@ public class CrimeFragment extends Fragment {
         //mDateButton.setText(android.text.format.DateFormat.format("EEEE MMM dd, yyy", mCrime.getDate()));
         updateDate();
 
-        /** show a dialog fragment **/
+        // Show a dialog fragment
         mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -114,7 +119,7 @@ public class CrimeFragment extends Fragment {
             }
         });
 
-        // THe solved checkbox
+        // The solved checkbox
         mSolvedCheckbox = (CheckBox) fragmentView.findViewById(R.id.crime_solved);
         mSolvedCheckbox.setChecked(mCrime.isSolved());
         mSolvedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
@@ -123,6 +128,43 @@ public class CrimeFragment extends Fragment {
                 mCrime.setSolved(isChecked);
             }
         });
+
+        // The report button
+        mReportButton = (Button)fragmentView.findViewById(R.id.crime_report);
+        mReportButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+                intent.putExtra(Intent.EXTRA_SUBJECT,
+                        getString(R.string.crime_report_subject));
+                intent = Intent.createChooser(intent, getString(R.string.send_report)); // user always chooses means of reporting
+                startActivity(intent);
+            }
+        });
+
+        // the Suspect button
+        final Intent pickContant = new Intent(Intent.ACTION_PICK,
+                ContactsContract.Contacts.CONTENT_URI);
+        // pickContant.addCategory(Intent.CATEGORY_HOME);       // test code for no contacts
+        mSuspectButton = (Button)fragmentView.findViewById(R.id.crime_suspect);
+        mSuspectButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivityForResult(pickContant, REQUEST_CONTACT);
+            }
+        });
+
+        if (mCrime.getSuspect() != null) {
+            mSuspectButton.setText(mCrime.getSuspect());
+        }
+
+        // if there is no Contacts app, don't crash the app - disable the button
+        PackageManager packageManager = getActivity().getPackageManager();
+        if (packageManager.resolveActivity(pickContant,
+                packageManager.MATCH_DEFAULT_ONLY) == null) {
+            mSuspectButton.setText(R.string.no_contacts_found);
+            mSuspectButton.setEnabled(false);
+        }
 
         return fragmentView;
     }
@@ -138,6 +180,29 @@ public class CrimeFragment extends Fragment {
                     .getSerializableExtra(DatePickerFragment.EXTRA_DATE);   // get the Date stored in the Intent's extra
             mCrime.setDate(date);
             updateDate();
+        } else if (requestCode == REQUEST_CONTACT && data != null) {
+            Uri contactUri = data.getData();
+            // Specify which fields you want your query to return values for
+            String[] queryFields = new String[] {
+                    ContactsContract.Contacts.DISPLAY_NAME
+            };
+            // Perform your query - the contactUri is like a Where clause
+            Cursor cursor = getActivity().getContentResolver()
+                    .query(contactUri, queryFields, null, null, null);
+            try {
+                // Double-check that you got results
+                if(cursor.getCount() == 0) {
+                    return;
+                }
+
+                // Pull out the first column of the first row of data - suspect's name
+                cursor.moveToFirst();
+                String suspect = cursor.getString(0);
+                mCrime.setSuspect(suspect);
+                mSuspectButton.setText(suspect + " suspected");
+            } finally {
+                cursor.close();
+            }
         }
     }
 
@@ -147,7 +212,7 @@ public class CrimeFragment extends Fragment {
 
     private String getCrimeReport() {
         String solvedString = null;
-        
+
         if(mCrime.isSolved()) {
             solvedString = getString(R.string.crime_report_solved);
         } else {
